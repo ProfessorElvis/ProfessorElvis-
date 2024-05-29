63866c5e85f4474068d1f4c8e22056d40ba2602d
ProfessorElvis <elvisrodrigues@prof.educacao.sp.gov.br>
2024-05-29 17:09:34 -0300

    create aquivo js e css

---
+++ Sketch.js
@@ -0,0 +1,23 @@
+//var bolinha
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 20;
+let raio = diametro / 2;
+
+//var oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//vel bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//placar jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
